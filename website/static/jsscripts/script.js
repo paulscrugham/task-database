665da7c6122f5bef314934d8b32443367e34f89f
Paul Scrugham <scrughap@flip2.engr.oregonstate.edu>
2021-08-09 18:13:00 -0700

    Add code comments to js script

--- website/static/jsscripts/script.js
+++ website/static/jsscripts/script.js
@@ -1,22 +1,21 @@
 // Source: adapted from https://www.w3schools.com/howto/howto_js_filter_table.asp
 
 function searchFilter(input_id, table_id, col_index) {
-  // Declare variables
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById(input_id);
-  filter = input.value.toUpperCase();
+  filter = input.value.toUpperCase();  // get upper case to match with table contents
   table = document.getElementById(table_id);
   tr = table.getElementsByTagName("tr");
 
-  // Loop through all table rows, and hide those who don't match the search query
+  // iterate over rows and hide row if search term is not present
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[col_index];
+    td = tr[i].getElementsByTagName("td")[col_index];  // select column to filter with its index
     if (td) {
       txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {  // get upper case to match with search term
         tr[i].style.display = "";
       } else {
-        tr[i].style.display = "none";
+        tr[i].style.display = "none";  // hides the row
       }
     }
   }
